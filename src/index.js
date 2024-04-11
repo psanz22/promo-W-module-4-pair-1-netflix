27fc76bf29297f008b92688217a77b82865a5190
Paula <psanzilundain@gmail.com>
2024-04-11 11:28:16 +0200

    ejs and template engines

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
-const express = require("express");
-const cors = require("cors");
-const mysql = require("mysql2/promise");
+const express = require('express');
+const cors = require('cors');
+const mysql = require('mysql2/promise');
 
 // create and config server
 const server = express();
 server.use(cors());
-server.use(express.json({ limit: "25mb" }));
+server.use(express.json({ limit: '25mb' }));
+server.set('view engine', 'ejs');
 
 async function getDBConnection() {
   const connection = await mysql.createConnection({
-    host: "sql.freedb.tech",
-    user: "freedb_admin_paula",
-    password: "GAj5@ct&C3a%ay4",
-    database: "freedb_netflix-pair-programming-team-1",
+    host: 'sql.freedb.tech',
+    user: 'freedb_admin_paula',
+    password: 'GAj5@ct&C3a%ay4',
+    database: 'freedb_netflix-pair-programming-team-1',
   });
   connection.connect();
   return connection;
@@ -24,14 +25,31 @@ server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
 
-server.get("/movies", async (req, res) => {
-  console.log("serán estos los query params?:", req.query);
+server.get('/', async (req, res) => {
   const connection = await getDBConnection();
-  const sql = "SELECT * FROM movies";
+  const sqlQuery = 'SELECT * FROM movies';
+  const [result] = await connection.query(sqlQuery);
+  console.log(result);
+  res.render('landing', { moviesList: result });
+});
+
+server.get('/movie/:movieId', async (req, res) => {
+  const connection = await getDBConnection();
+  const foundMovie = 'SELECT * FROM movies WHERE id_movies=?';
+  const [result] = await connection.query(foundMovie, [req.params.movieId]);
+  console.log(result);
+  connection.end();
+  res.render('movie', { movies: result[0] });
+});
+
+server.get('/movies', async (req, res) => {
+  console.log('serán estos los query params?:', req.query);
+  const connection = await getDBConnection();
+  const sql = 'SELECT * FROM movies';
   const [moviesResult] = await connection.query(sql); // devuelve un array con dos elementos
   connection.end();
   res.status(200).json({
-    status: "success",
+    status: 'success',
     message: moviesResult,
   });
 });
